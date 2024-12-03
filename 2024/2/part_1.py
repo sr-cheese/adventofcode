49206b2fdd7503186cd543da0ccc1a09be3c9308
Javier Argüeso Soto <javierarguesosoto@gmail.com>
2024-12-03 11:12:02 +0100

    day 2: part 2 failed

--- 2024/2/part_1.py
+++ 2024/2/part_1.py
@@ -17,29 +17,25 @@
 #     8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
 #     1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
 
-import re
-
 # read input file
-inputs = open('input', 'r')
-lines  = inputs.readlines()
-inputs.close()
+with open('example_input', 'r') as file :
+    content = file.read()
+    lines   = content.strip().split('\n')
+    result  = [ list( map( int, line.split() ) ) for line in lines ]
 
-safe_reports = 0
+safe_reports      = 0
 
-for line in lines :
-    list_line = re.split(r' ', line)
-    numeric_line = list(map(int, list_line))
+for line in result :
+    count         = 1
+    last_status   = 0
+    last_level    = line[0]
+    actual_status = last_level - line[1]
 
-    count = 1
-    last_status = 0
-    last_value = numeric_line[0]
-    actual_status = last_value - numeric_line[1]
-
-    while count < len(numeric_line) :
-        last_value = numeric_line[count - 1]
-        actual_value = numeric_line[count]
-        last_status = actual_status
-        actual_status = last_value - actual_value
+    while count < len(line) :
+        last_level    = line[count - 1]
+        actual_level  = line[count]
+        last_status   = actual_status
+        actual_status = last_level - actual_level
 
         if actual_status == 0 :
             break
@@ -50,12 +46,12 @@ for line in lines :
         if actual_status > 0 and last_status < 0 :
             break
 
-        if abs(actual_value - last_value) > 3 :
+        if abs(actual_level - last_level) > 3 :
             break
 
         count += 1
 
-    if count == len(numeric_line) :
+    if count == len(line) :
         safe_reports += 1
 
 print(safe_reports)
--- 2024/2/part_2.py
+++ 2024/2/part_2.py
@@ -1,8 +1,68 @@
+# example_imput:
+#   7 6 4 2 1
+#   1 2 7 8 9
+#   9 7 6 2 1
+#   1 3 2 4 5
+#   8 6 4 4 1
+#   1 3 6 7 9
 
+# Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+# More of the above example's reports are now safe:
+#   7 6 4 2 1: Safe without removing any level.
+#   1 2 7 8 9: Unsafe regardless of which level is removed.
+#   9 7 6 2 1: Unsafe regardless of which level is removed.
+#   1 3 2 4 5: Safe by removing the second level, 3.
+#   8 6 4 4 1: Safe by removing the third level, 4.
+#   1 3 6 7 9: Safe without removing any level.
 
-import re
+import copy
 
 # read input file
-inputs = open('input', 'r')
-lines  = inputs.readlines()
-inputs.close()
+with open('input', 'r') as file :
+    content = file.read()
+    lines   = content.strip().split('\n')
+    result  = [ list( map( int, line.split() ) ) for line in lines ]
+
+safe_reports = 0
+
+def is_safe(actual_status, last_status, actual_level, last_level) :
+    if actual_status == 0 :
+        return False
+    if actual_status < 0 and last_status > 0 :
+        return False
+    if actual_status > 0 and last_status < 0 :
+        return False
+    if abs(actual_level - last_level) > 3 :
+        return False
+    return True
+
+def check_line(line, removed_level) :
+    count         = 1
+    last_status   = 0
+    last_level    = line[0]
+    actual_status = last_level - line[1]
+
+    while count < len(line) :
+        last_level    = line[count - 1]
+        actual_level  = line[count]
+        last_status   = actual_status
+        actual_status = last_level - actual_level
+
+        if not(is_safe(actual_status, last_status, actual_level, last_level)) :
+            if removed_level == 0 :
+                dampener_line = copy.deepcopy(line)
+                dampener_line.pop(count)
+                return check_line(dampener_line, 1)
+            else :
+                break
+        count += 1
+
+    if count == len(line) :
+        return 1
+    print(f'linea {line} invalida')
+    return 0
+
+for line in result :
+    safe_reports += check_line(line, 0)
+
+print(safe_reports)
