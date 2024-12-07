def0b90a3ad157a955742a364c6d69371796f836
Javier Argüeso Soto <javierarguesosoto@gmail.com>
2024-12-07 18:46:36 +0100

    [ UPDATE ] [ COMPLETE ] day 6 part 1

--- 2024/06/part_1.py
+++ 2024/06/part_1.py
@@ -1,37 +1,83 @@
 ###################################################################################### imports
 
 import os
+import re
 import time
 import numpy as np
 
 #################################################################################### functions
 
-def check_next(actual_map, next_pos) :
-    if next_pos[0] > 0 and next_pos[0] < len(actual_map) and next_pos[1] > 0 and next_pos[1] < len(actual_map[0]) :
-        if actual_map[next_pos[0]][next_pos[1]] == '#' :
-            actual_map[next_pos[0]][next_pos[1]] = '>'
-            return actual_map
-
 def move_up(actual_map, possition) :
-    return 0
+    if possition[0] - 1 >= 0 :
+        if actual_map[possition[0] - 1][possition[1]] == '#' :
+            actual_map[possition[0]][possition[1]] = '>'
+            return actual_map
+        else :
+            actual_map[possition[0] - 1][possition[1]] = '^'
+            actual_map[possition[0]][possition[1]] = 'X'
+            return actual_map
+    else :
+        actual_map[possition[0]][possition[1]] = 'X'
+        return actual_map
 
 def move_right(actual_map, possition) :
-    return 0
+    if possition[1] + 1 < len(actual_map[0]) :
+        if actual_map[possition[0]][possition[1] + 1] == '#' :
+            actual_map[possition[0]][possition[1]] = 'v'
+            return actual_map
+        else :
+            actual_map[possition[0]][possition[1] + 1] = '>'
+            actual_map[possition[0]][possition[1]] = 'X'
+            return actual_map
+    else :
+        actual_map[possition[0]][possition[1]] = 'X'
+        return actual_map
 
 def move_down(actual_map, possition) :
-    return 0
+    if possition[0] + 1 < len(actual_map) :
+        if actual_map[possition[0] + 1][possition[1]] == '#' :
+            actual_map[possition[0]][possition[1]] = '<'
+            return actual_map
+        else :
+            actual_map[possition[0] + 1][possition[1]] = 'v'
+            actual_map[possition[0]][possition[1]] = 'X'
+            return actual_map
+    else :
+        actual_map[possition[0]][possition[1]] = 'X'
+        return actual_map
 
 def move_left(actual_map, possition) :
-    return 0
+    if possition[1] - 1 >= 0 :
+        if actual_map[possition[0]][possition[1] - 1] == '#' :
+            actual_map[possition[0]][possition[1]] = '^'
+            return actual_map
+        else :
+            actual_map[possition[0]][possition[1] - 1] = '<'
+            actual_map[possition[0]][possition[1]] = 'X'
+            return actual_map
+    else :
+        actual_map[possition[0]][possition[1]] = 'X'
+        return actual_map
 
 def move(actual_map) :
-    array_row, array_column = np.where(actual_map == '^')
+
+    if '^' in actual_map :
+        array_row, array_column = np.where(actual_map == '^')
+    elif '>' in actual_map :
+        array_row, array_column = np.where(actual_map == '>')
+    elif 'v' in actual_map :
+        array_row, array_column = np.where(actual_map == 'v')
+    elif '<' in actual_map :
+        array_row, array_column = np.where(actual_map == '<')
+    else :
+        return actual_map
+
     row = array_row[0]
     column = array_column[0]
 
-    os.system('cls||clear')
-    print(actual_map)
-    time.sleep(1)
+    # os.system('cls||clear')
+    # print(actual_map)
+    # time.sleep(0.2)
 
     if row > 0 and row < len(actual_map) and column > 0 and column < len(actual_map[0]) :
         match actual_map[row][column] :
@@ -44,14 +90,20 @@ def move(actual_map) :
             case '<' :
                 new_map = move_left(actual_map, [row, column])
         
-        return new_map
-        # return move(new_map)
-    return actual_map
+    return new_map
 
 ######################################################################################## main
 
-with open('example_input', 'r') as file :
+with open('input', 'r') as file :
     content = file.read()
     lines   = content.strip().split('\n')
 
-move(np.array([ list(line) for line in lines ]))
+actual_map = np.array([ list(line) for line in lines ])
+
+while '^' in actual_map or '>' in actual_map or 'v' in actual_map or '<' in actual_map :
+    actual_map = move(actual_map)
+
+# os.system('cls||clear')
+# print(actual_map)
+
+print(sum([ len(re.findall('X', ''.join(line))) for line in actual_map ]))
